2820c2bd851da4bc7b4334fdc9de34a5609f9425
ThonkikhDanil <daniltonkih575@outlook.com>
2025-01-17 17:17:11 +0500

    Added HeaderBottom component

---
+++ resources/js/Components/HeaderBottom.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { Link } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <div class="bg-[#eb3e32]">
+        <div class="container flex items-center justify-center mx-auto uppercase">
+            <Link :href="route('home')" class="p-4">Главная</Link>
+            <Link class="p-4">Отзывы</Link>
+            <Link class="p-4">О нас</Link>
+            <Link class="p-4">Магазин</Link>
+            <Link class="p-4">Написать нам</Link>
+        </div>
+    </div>
+</template>
--- resources/js/Layouts/Layout.vue
+++ resources/js/Layouts/Layout.vue
@@ -1,10 +1,12 @@
 <script setup>
 import HeaderTop from '@/Components/HeaderTop.vue';
 import HeaderMiddle from '@/Components/HeaderMiddle.vue';
+import HeaderBottom from '@/Components/HeaderBottom.vue';
 </script>
 
 <template>
     <HeaderTop />
     <HeaderMiddle />
+    <HeaderBottom />
     <slot />
 </template>

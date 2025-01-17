5eb77c6c959d94eafd61747cfd727bfbbf8ba907
ThonkikhDanil <daniltonkih575@outlook.com>
2025-01-17 15:52:12 +0500

    Added Flaticon and some icons from Flaticon

--- resources/js/Components/HeaderMiddle.vue
+++ resources/js/Components/HeaderMiddle.vue
@@ -1,12 +1,10 @@
 <script setup>
 import { Link } from '@inertiajs/vue3';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
 </script>
 
 <template>
     <div class="py-6">
-        <div class="container flex justify-between mx-auto">
+        <div class="container flex items-center justify-between mx-auto">
             <Link :href="route('home')" class="text-[2rem] font-semibold">
                 <span class="text-black">TREND</span>
                 <span class="text-[#eb3e32]">CRAFT</span>
@@ -15,12 +13,12 @@ import { faSearch } from '@fortawesome/free-solid-svg-icons';
                 <input type="text" name="product_name" id="product_name" placeholder="Поиск"
                     class="border-2 border-solid border-[#e8e8e8] rounded-s-lg w-[450px] text-[14px] border-r-0">
                 <button type="submit" class="rounded-e-lg bg-[#eb3e32] flex items-center px-2 text-white hover:bg-[#333131] w-[65px] justify-center">
-                    <FontAwesomeIcon :icon="faSearch" />
+                    <i class="fi fi-rr-search" />
                 </button>
             </form>
             <div class="flex">
-                <div class="pr-4">Желаемое</div>
-                <div>Корзина</div>
+                <i class="pr-4 text-2xl fi fi-rr-heart" />
+                <i class="text-2xl fi fi-rr-shopping-bag" />
             </div>
         </div>
     </div>
--- resources/js/Components/HeaderTop.vue
+++ resources/js/Components/HeaderTop.vue
@@ -1,14 +1,13 @@
 <script setup>
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faUser } from '@fortawesome/free-solid-svg-icons';
+
 </script>
 
 <template>
     <div class="bg-[#f4f4f4] py-2">
-        <div class="container flex justify-between mx-auto">
+        <div class="container flex items-center justify-between mx-auto">
             <div>Еженедельные скидки только у нас!</div>
             <div>
-                <FontAwesomeIcon :icon="faUser" class="text-[#eb3e32] mr-3" />
+                <i class="fi fi-ss-user text-[#eb3e32] mr-3" />
                 Войти
             </div>
         </div>

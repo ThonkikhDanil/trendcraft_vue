4e1f32a041b1a4f7ca6316d88f147d40b2ef841e
ThonkikhDanil <daniltonkih575@outlook.com>
2025-01-17 16:32:50 +0500

    Route home moved to /

--- routes/web.php
+++ routes/web.php
@@ -6,14 +6,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+Route::get('/', HomeController::class)->name('home');
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
@@ -24,7 +17,5 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
-Route::get('/home', HomeController::class)->name('home');
 
 require __DIR__.'/auth.php';
